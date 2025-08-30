a622753a19204ad2aa78db88a82697f13692a312
loeda4789 <maxloeder@web.de>
2025-08-30 21:38:58 +0200

    Fix corrupted page.tsx and disable Webseite-Designer permanently - Restore working page.tsx from earlier commit - ConfiguratorButton shows disabled state and blocks Designer access - Build is working again

--- components/ConfiguratorButton.tsx
+++ components/ConfiguratorButton.tsx
@@ -5,6 +5,20 @@ interface ConfiguratorButtonProps {
 }
 
 export default function ConfiguratorButton({ onClick }: ConfiguratorButtonProps) {
-  // Website-Konfigurator ist komplett deaktiviert
-  return null
+  // Website-Konfigurator ist komplett deaktiviert - zeige Button aber ohne Funktion
+  return (
+    <div className="fixed bottom-4 left-4 z-50">
+      <button
+        onClick={() => {
+          console.log('Webseite-Designer ist deaktiviert');
+          // onClick() wird NICHT aufgerufen - verhindert das Öffnen des Designers
+        }}
+        className="bg-gray-400 text-white px-3 py-2 rounded-lg cursor-not-allowed opacity-50 text-sm"
+        title="Webseite-Designer ist deaktiviert"
+        disabled
+      >
+        🔒 Designer deaktiviert
+      </button>
+    </div>
+  )
 } 
