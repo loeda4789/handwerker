565952b8ba2587738ccaf9f19686d9d917e75d5a
loeda4789 <maxloeder@web.de>
2025-08-30 21:25:03 +0200

    Completely disable ConfiguratorButton - Button now returns null - Webseite-Designer cannot be opened anymore - Clean solution without modifying main page

--- components/ConfiguratorButton.tsx
+++ components/ConfiguratorButton.tsx
@@ -5,16 +5,6 @@ interface ConfiguratorButtonProps {
 }
 
 export default function ConfiguratorButton({ onClick }: ConfiguratorButtonProps) {
-  // Website-Konfigurator ist deaktiviert
-  return (
-    <div className="fixed bottom-4 left-4 z-50">
-      <button
-        disabled
-        className="bg-gray-400 text-white px-4 py-2 rounded-lg cursor-not-allowed opacity-50"
-        title="Website-Konfigurator ist deaktiviert"
-      >
-        ⚠️ Konfigurator deaktiviert
-      </button>
-    </div>
-  )
+  // Website-Konfigurator ist komplett deaktiviert
+  return null
 } 
