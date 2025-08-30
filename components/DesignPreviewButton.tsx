bb8f07e131818a862f1fb56d99437b8476984f43
loeda4789 <maxloeder@web.de>
2025-08-30 21:07:41 +0200

    Clean up unused imports in disabled components - Remove unused imports from DesignPreviewButton - Remove unused imports from ConfiguratorButton - Both components now return null and are fully disabled

--- components/DesignPreviewButton.tsx
+++ components/DesignPreviewButton.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import DesignPreview from './DesignPreview'
-
 export default function DesignPreviewButton() {
   // Webseite-Designer ist ausgeblendet
   return null
