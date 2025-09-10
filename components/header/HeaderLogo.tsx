4009dd315681217f33e619a4b7a385fe1b4ef0d7
loeda4789 <maxloeder@web.de>
2025-09-10 21:21:39 +0200

    feat: Firmennamen aus Header entfernt
    - Firmennamen-Text aus HeaderLogo entfernt
    - Nur noch Icon mit Initialen wird angezeigt
    - space-x-3 Klasse entfernt (nicht mehr benötigt)
    - hidden sm:block Klasse entfernt (nicht mehr benötigt)
    - Logo bleibt kompakt und minimalistisch
    - Kontakt bereits für Professional Package entfernt

--- components/header/HeaderLogo.tsx
+++ components/header/HeaderLogo.tsx
@@ -17,16 +17,13 @@ export default function HeaderLogo({ logoStyle, companyName }: HeaderLogoProps) 
   }
 
   return (
-    <Link href="/" className="flex items-center space-x-3">
+    <Link href="/" className="flex items-center">
       <div 
         className={`w-10 h-10 ${logoStyle} border-2 border-current flex items-center justify-center logo-font text-lg`}
         style={{ borderRadius: 'var(--radius-button)' }}
       >
         {getCompanyInitials(companyName)}
       </div>
-      <span className={`text-2xl logo-font ${logoStyle} hidden sm:block`}>
-        {companyName}
-      </span>
     </Link>
   )
 }
